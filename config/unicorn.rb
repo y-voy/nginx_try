fe1d42eb2091312f1b83f85742bcfa4df959e305
yuji moriya <voyager.nasa0928@gmail.com>
2021-03-20 20:44:11 +0900

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,5 @@
-rails_root = File.expand_path('../../', __FILE__)
-
-worker_processes 2
-working_directory rails_root
-
-listen "#{rails_root}/tmp/unicorn.sock"
-pid "#{rails_root}/tmp/unicorn.pid"
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
+require 'fileutils'
+listen '/tmp/nginx.socket'
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
+end
